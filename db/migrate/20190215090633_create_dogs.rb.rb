2be946bb4ee2e8dfde96d6dc7a89a904fae50b47
Azraf Chowdhury <achowdhur@ltu.edu>
2019-02-15 09:08:12 +0000

    Automatically backed up by Learn

--- db/migrate/20190215090633_create_dogs.rb.rb
+++ db/migrate/20190215090633_create_dogs.rb.rb
@@ -1,10 +1,11 @@
-class CreateDogs.rb < ActiveRecord::Migration
+class CreateDogs < ActiveRecord::Migration
   def up
-  create_table :dogs do |t|
-    t.string :name
-    t.string :breed
+    create_table :dogs do |t|
+      t.string :name
+      t.string :breed
+    end
   end
-
+ 
   def down
     drop_table :dogs
   end
